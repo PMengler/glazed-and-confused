740cb262d83223aa2a763014d357027d357d8c1c
PMengler <114950259+PMengler@users.noreply.github.com>
2023-04-06 20:39:45 -0700

    bug fixing

--- server/server.js
+++ server/server.js
@@ -23,7 +23,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
@@ -38,5 +37,4 @@ const startApolloServer = async (typeDefs, resolvers) => {
   });
 };
 
-// Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
