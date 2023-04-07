b21e49e198fc82aec5f3e6fac3e69d374cc156a3
Nick Morris <nicholasmorris2599@gmail.com>
2023-04-06 20:53:34 -0700

    User password is hashed with salt rounds.

--- server/models/User.js
+++ server/models/User.js
@@ -27,6 +27,15 @@ const userSchema = new Schema({
     ],
 });
 
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.pass, saltRounds);
+    }
+
+    next();
+})
+
 const User = model('User', userSchema);
 
 module.exports = User;
