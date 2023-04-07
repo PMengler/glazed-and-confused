7e70e9f229ebbe5b5d2464f6a8a04dc7b0cb9822
PMengler <114950259+PMengler@users.noreply.github.com>
2023-04-07 10:23:41 -0700

    had to redo changes to schema files after revert

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,17 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+  }
+
+  type Donut {
+  }
+
+  type Query {
+  }
+
+  type Mutation {
+  }
+`;
+
+module.exports = typeDefs;
