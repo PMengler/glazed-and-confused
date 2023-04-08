a0040de80a6ec3f4dd0f99ab6457f3096596995b
Nick Morris <nicholasmorris2599@gmail.com>
2023-04-07 17:57:36 -0700

    Laying out basic structure for the main signup export utilizing state.

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,3 +1,29 @@
 import React, { useState } from 'react';
 
-import { useMutation } from '@apollo/client'
+import { useMutation } from '@apollo/client';
+
+// Still need to create mutations
+// import {  } from '../utils/mutations'
+
+// Still need to create the util functions
+// import Auth from '../utils/auth';
+
+const Signup = () => {
+    const [formState, setFormState] = useState({
+        username: '',
+        email: '',
+        password: '',
+    });
+
+    const [addUser, { error, data }] = useMutation(
+        // Need to create mutation for adding a user
+    );
+
+    return (
+        <>
+        <main>
+            
+        </main>
+        </>
+    );
+};
