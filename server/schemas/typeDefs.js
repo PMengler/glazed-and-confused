75d2d4064661c525e667e5040549769514aee8dc
Nick Morris <nicholasmorris2599@gmail.com>
2023-04-07 18:34:26 -0700

    Added more type definitions related to the User.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -13,4 +13,20 @@ const typeDefs = gql`
         _id: ID
         // Need to iron out what a donut has
     }
+
+    type Auth {
+        token: ID
+        user: User
+    }
+
+    type Query {
+        users: [User]
+        user(username: String!): User
+        me: User
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+    }
 `
