da27a3f1942d5fbc2606b1d85afec275d92eab9e
Nick Morris <nicholasmorris2599@gmail.com>
2023-04-09 12:40:07 -0700

    Finished Donut model.

--- server/models/Donut.js
+++ server/models/Donut.js
@@ -1,7 +1,26 @@
 const { Schema, model } = require('mongoose');
 
 const donutSchema = new Schema({
-    
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        require: true,
+        trim: true,
+    },
+    image: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    contains: {
+        type: String,
+        required: true,
+    },
 });
 
 const Donut = model('Donut', donutSchema);
