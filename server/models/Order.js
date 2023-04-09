2dd5fa78b5f2844d5f5439e99c7dc2f72fa2a408
Nick Morris <nicholasmorris2599@gmail.com>
2023-04-09 15:57:57 -0700

    Completted order model.

--- server/models/Order.js
+++ server/models/Order.js
@@ -1,15 +1,30 @@
 const { Schema, model } = require('mongoose');
 
-const orderSchema = new Schema({
-    totalPrice: {
-        // Use of aggretates?
+const orderSchema = new Schema(
+    {
+        totalPrice: {
+            // Use of aggretates?
+        },
+        boxes: {
+            type: Schema.Types.ObjectId,
+            ref: 'Boxes'
+        },
     },
-    boxes: {
-        type: Schema.Types.ObjectId,
-        ref: 'Boxes'
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
     },
-});
+);
 
+
+orderSchema
+    .virtual('orderTotal')
+    .get(function() {
+        return this.boxes.totalPrice;
+    });
+    
 const Order = model('Order', orderSchema);
 
 module.exports = Order;
