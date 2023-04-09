3e3e60f2f6c7c3537eb289aece5174a56ff8c3fe
Nick Morris <nicholasmorris2599@gmail.com>
2023-04-09 14:40:17 -0700

    Medium box setup with validation. Once again need to connect with team on correct validation.

--- server/models/MediumBox.js
+++ server/models/MediumBox.js
@@ -1,8 +1,21 @@
 const { Schema, model } = require('mongoose');
 
 const mediumBoxSchema = new Schema({
-
+    quantity: 6,
+    price: 'some price',
+    donuts: {
+        type: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Donut',
+        }],
+        validate: [amount, 'Must have 6 donuts in this box.']
+    } 
 });
+
+// Need to confirm 
+function amount(donut) {
+    return donut.length = 6
+};
 
 const MediumBox = model('MediumBox', mediumBoxSchema);
 
--- server/models/SmallBox.js
+++ server/models/SmallBox.js
@@ -1,21 +1,21 @@
 const { Schema, model } = require('mongoose');
 
 const smallBoxSchema = new Schema({
-    size: 4,
+    quantity: 4,
     price: 'some price',
     donuts: {
         type: [{
             type: Schema.Types.ObjectId,
             ref: 'Donut',
         }],
-        validate: [amount, 'Must have 4 donuts.']
+        validate: [amount, 'Must have 4 donuts in this box.']
     }   
 });
 
 // Need to confirm 
 function amount(donut) {
     return donut.length = 4
-}
+};
 
 const SmallBox = model('SmallBox', smallBoxSchema);
 
