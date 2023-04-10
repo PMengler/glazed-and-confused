3a8e444bb3c3623573e80754fb2b4b7e5c124507
Nick Morris <nicholasmorris2599@gmail.com>
2023-04-10 15:53:40 -0700

    Proper imports from react and apollo connected.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Router as Router, Routes, Route } from 'react-router-dom';
 import '../src/styles/';
 
+import { setContext } from '@apollo/client/link/context';
+import { 
+  ApolloProvider,
+  ApolloClient, 
+  InMemoryCache, 
+  createHttpLink } from '@apollo/client';
+  
 import Header from './components/Header';
-import { ApolloProvider } from '@apollo/client';
-import { Router } from 'react-router-dom';
+
+const httpLink = createHttpLink({
+  uri: '/graphql'
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Hearer ${token}` : '',
+    },
+  };
+});
 
 const client = new ApolloClient({
-  
-})
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
