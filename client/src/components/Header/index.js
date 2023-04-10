8067e2f2ddf9cedb2e590194f5c01641eb485171
Nick Morris <nicholasmorris2599@gmail.com>
2023-04-10 16:18:27 -0700

    Continuing to merge css code into the specific react header component.

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -6,13 +6,28 @@ import logoPic from '../../assets/gc-logo.png';
 import logoMobile from '../../assets/gc-logo-mobile.png';
 
 const Header = () => {
+    const styles = {
+        a: {
+            textDecoration: 'none',
+        },
+        body: {
+            backgroundColor: 'rgb(235, 235, 235)',
+        },
+        nav: {
+            display: 'flex',
+            justifyContent: 'center',
+            height: '75px',
+            backgroundColor: 'white',
+            filter: 'drop-shadow(0px 3px 9px rgba(0, 0, 0, 0.25))',
+        }
+    }
     // Functions that will be used in the header
 
     return (
-        <header className="header">
-            <nav>
+        <header className="header" style={styles.body}>
+            <nav style={styles.nav}>
                 <ul>
-                    <li><a className="navlink" href="#theflavors">THE FLAVORS</a></li>
+                    <li><a className="navlink" href="#theflavors" style={styles.a}>THE FLAVORS</a></li>
                     <li><a className="navlink" href="#contactus">CONTACT US</a></li>
                     <li className="header-logo"><img src={logoPic} alt='logoPic'></img></li>
                     <li><a className="navlink" href="#ourstory">OUR STORY</a></li>
