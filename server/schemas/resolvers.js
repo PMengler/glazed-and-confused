60a1f6745589c7cf8f34869df97040e929224588
Nick Morris <nicholasmorris2599@gmail.com>
2023-04-10 19:48:52 -0700

    Now utilizing sign token.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -20,7 +20,7 @@ const resolvers = {
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
-      const token = AuthService(user);
+      const token = signToken(user);
       return { token, user };
     },
     login: async (parent, { email, password }) => {
