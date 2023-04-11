98d4acf843e445ef6a68310ec581f47e85edba27
Nick Morris <nicholasmorris2599@gmail.com>
2023-04-10 21:33:25 -0700

    Apollo queries fully functional for testing resolvers with type definitions!

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -32,7 +32,7 @@ const resolvers = {
       if (!correctPw) {
         throw new AuthenticationError('Incorrect credentials');
       }
-      const token = AuthService(user);
+      const token = signToken(user);
       return { token, user };
     },
   },
