62e4896cf0fa7cc0e8b429feda51ab33a1444d1a
Savannah Vel <savannah.vel@vivint.com>
2023-04-10 20:38:37 -0700

    fixed jsx in hero component

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -2,17 +2,17 @@ import React from 'react';
 
 const Hero = () => {
     return (
-        <section class="heropic">
-            <div class="hero-left">
-                <div class="hero-label">
+        <section className="heropic">
+            <div className="hero-left">
+                <div className="hero-label">
                     <h1>Donut Panic!<br/>
                         There’s plenty for everyone.</h1>
                     <p>Let's find that perfect flavor.</p>
-                    <button class="btn-lrg btn-black">START YOUR ORDER</button>
+                    <button className="btn-lrg btn-black">START YOUR ORDER</button>
                 </div>
             </div>
-            <div class="hero-right">
-                <div class="hero-shape"></div>
+            <div className="hero-right">
+                <div className="hero-shape"></div>
             </div>
        </section> 
     )
