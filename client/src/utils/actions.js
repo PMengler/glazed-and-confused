8eb8e2984f5621b9e76300a36566f1c382071eb8
PMengler <114950259+PMengler@users.noreply.github.com>
2023-04-13 13:42:04 -0700

    updating frontend mutations and actions

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -3,6 +3,7 @@ export const UPDATE_USER = 'UPDATE_USER';
 export const LOGIN = 'LOGIN';
 export const NEW_ORDER = 'NEW_ORDER';
 export const ADD_DONUT_TO_ORDER = 'ADD_DONUT_TO_ORDER';
+export const REMOVE_DONUT_FROM_ORDER = 'REMOVE_DONUT_FROM_ORDER';
 
 // export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
 // export const ADD_TO_CART = "ADD_TO_CART";
