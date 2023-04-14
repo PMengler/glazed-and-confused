5c460ab782df531c91bdd360629c11d94d8ac000
Nick Morris <nicholasmorris2599@gmail.com>
2023-04-13 20:39:48 -0700

    Adding component function with base functionality.

--- client/src/components/Popup/index.js
+++ client/src/components/Popup/index.js
@@ -3,3 +3,27 @@ import { Link } from 'react-router-dom';
 import { useStoreContext } from '../../utils/GlobalState';
 import { ADD_DONUT_TO_ORDER, UPDATE_ORDER_QUANTITY} from '../../utils/actions';
 import { idbPromies } from '../../utils/helpers';
+
+function Popup(donut) {
+    const [state, dispatch] = useStoreContext();
+
+    const {
+        _id,
+        name,
+        description,
+        price,
+        image
+    } = donut;
+
+    const { order } = state
+    
+    // Functions for handling donut being added to order
+
+    return (
+        <>
+
+        </>
+    );
+}
+
+export default Popup;
