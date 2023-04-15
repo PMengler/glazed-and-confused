51c6e2d43d9994c33cae28cd20bec4fbeaccc69d
Nick Morris <nicholasmorris2599@gmail.com>
2023-04-15 12:46:17 -0700

    Added a check for token expiration.

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -20,7 +20,14 @@ class AuthService {
     }
 
     isTokenExpired(token) {
-        
+        try {
+            const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            } else return false;
+        } catch (err) {
+            return false;
+        }
     }
 }
 
