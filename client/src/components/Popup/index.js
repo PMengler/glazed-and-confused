eaa5704880a18fe7433e5301651c66f528eb1133
Nick Morris <nicholasmorris2599@gmail.com>
2023-04-16 13:50:23 -0700

    Functional popup display that adds to the users current order.

--- client/src/components/Popup/index.js
+++ client/src/components/Popup/index.js
@@ -21,12 +21,12 @@ function Popup() {
         if (donuts.length) {
             setCurrentDonut(donuts.find((donut) => donut._id === id));
         }
-    })
+    }, [data, loading, dispatch]);
 
 
     // Functions for handling donut being added to order
     const addDonutToOrder = () => {
-        const donutInOrder = order.find((orderItem) => orderItem._id === _id)
+        const donutInOrder = order.find((orderItem) => orderItem._id === currentDonut._id)
         if (donutInOrder) {
             dispatch({
                 type: UPDATE_ORDER_QUANTITY,
@@ -40,9 +40,9 @@ function Popup() {
         } else {
             dispatch({
                 type: ADD_DONUT_TO_ORDER,
-                donut: { ...donut, purchaseQuantity: 1 }
+                donut: { ...currentDonut, purchaseQuantity: 1 }
             });
-            idbPromise('order', 'put', { ...donut, purchaseQuantity: 1 });
+            idbPromise('order', 'put', { ...currentDonut, purchaseQuantity: 1 });
         }
     }
 
@@ -51,24 +51,28 @@ function Popup() {
             <div className="popup-window">
                 <section className="popup-container">
                     <div className="popup-swirl"><img src={"/images/swirl-blue.svg"} alt="swirl" width="100%"></img></div>
-                    <a className="close" href="#home">CLOSE</a>
+                    <Link to={'/flavors'}>
+                        <p className="close">CLOSE</p>
+                    </Link>
                     <div className="popup-section">
                         <div className="donut-pic">
-                            <img src={`/images/${donut.image}`}></img>
+                            <img src={`/images/${currentDonut.image}`}></img>
                         </div>
                         <div className="popup-info">
-                            <h2>{donut.name}</h2>
-                            <p className="popup-p">{donut.description}
+                            <h2>{currentDonut.name}</h2>
+                            <p className="popup-p">{currentDonut.description}
                             </p>
-                            </div>
                         </div>
-                        <div className="popup-footer">
-                            <div className="ingredients"> icon Contains:<span className='allergies'>Milk, Wheat, Egg</span></div>
-                            <div className="popup-addtocart-box">
-                                <span className="popup-addtocart">ADD TO CART</span>
+                    </div>
+                    <div className="popup-footer">
+                        <div className="ingredients"> icon Contains:<span className='allergies'>Milk, Wheat, Egg</span></div>
+                        <div className="popup-addtocart-box">
+                            <span className="popup-addtocart">ADD TO CART</span>
+                            <Link to={'/flavors'}>
                                 <button className="popup-btn" onClick={addDonutToOrder}>icon</button>
-                            </div>
+                            </Link>
                         </div>
+                    </div>
                 </section>
             </div>
         </>
