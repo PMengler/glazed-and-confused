59e9ebe16fa22ff37a8cba27d2923b343c875888
Nick Morris <nicholasmorris2599@gmail.com>
2023-04-17 14:41:41 -0700

    Adding code from Popup which uses the same add to state function that I need.

--- client/src/components/PastOrderCard/index.js
+++ client/src/components/PastOrderCard/index.js
@@ -1,13 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PastOrderItem from '../PastOrderItem';
 import PastOrderEmpty from '../PastOrderEmpty';
-import { ADD_MULTIPLE_TO_ORDER } from "../../utils/actions";
+import { ADD_MULTIPLE_TO_ORDER, ADD_ORDER_TO_USERORDER } from "../../utils/actions";
+import { useQuery } from '@apollo/client';
 import { idbPromise } from '../../utils/helpers';
 import { useStoreContext } from '../../utils/GlobalState';
 
 function PastOrderCard() {
 
     const [state, dispatch] = useStoreContext();
+    const [currentOrder, setCurrentOrder] = useState({});
+    const { userOrders } = state;
+    const { loading, data } = useQuery(ADD_ORDER_TO_USERORDER);
 
     const orderNumber = 0;
     console.log(state)
@@ -23,6 +27,42 @@ function PastOrderCard() {
         }
 
     }, [dispatch]);
+
+    // useEffect(() => {
+    //     if (data) {
+
+    //     }
+    // })
+
+
+
+
+    const addOrderToUserOrder = () => {
+        const orderInUserOrder = userOrders.find((order) => order._id === )
+        if (donutInOrder) {
+            dispatch({
+                type: UPDATE_ORDER_QUANTITY,
+                _id: _id,
+                purchaseQuantity: parseInt(donutInOrder.purchaseQuantity) + 1
+            });
+            idbPromise('order', 'put', {
+                ...donutInOrder,
+                purchaseQuantity: parseInt(donutInOrder.purchaseQuantity) + 1
+            });
+        } else {
+            dispatch({
+                type: ADD_DONUT_TO_ORDER,
+                donut: { ...currentDonut, purchaseQuantity: 1 }
+            });
+            idbPromise('order', 'put', { ...currentDonut, purchaseQuantity: 1 });
+        }
+    }
+
+
+
+
+
+    
 
     return (
         // Will handle if order is empty
