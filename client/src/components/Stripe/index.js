0cb4ec07aac19ebea8d260bc5180b0cc2423de84
BJ Smith <bryanjeremysmith@gmail.com>
2023-04-17 13:02:46 -0700

    created a new "donut" for 2.99, used the state.order to get number of donuts

--- client/src/components/Stripe/index.js
+++ client/src/components/Stripe/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 // import {stripeProducts} from '@stripe/react-stripe-js';
 import { Elements, useStripe } from '@stripe/react-stripe-js';
+import { useStoreContext } from "../../utils/GlobalState";
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
 function CheckoutButton() {
   const stripe = useStripe();
+  const [state, dispatch] = useStoreContext();
 
   const handleClick = async () => {
+
+    let numberOfDonuts = 0;
+    for(let i = 0; i < state.order.length; i++){
+      numberOfDonuts = numberOfDonuts + state.order[i].purchaseQuantity;
+    }
+    
     //TODOBJS
     // const url = new URL(context.headers.referer).originF;
     //const order = new Order({ products: args.products });
@@ -37,7 +45,7 @@ function CheckoutButton() {
     // }
 
     const { error } = await stripe.redirectToCheckout({
-      lineItems: [{ price: 'price_1Mw96NBFISeLbxNY0T43FU7Z', quantity: 20}],
+      lineItems: [{ price: 'price_1Mxy4uBFISeLbxNY2XKAcfut', quantity: numberOfDonuts}],
       mode: 'payment',
 
       //For deployment
