01d89dbeb961f338be2afa586f26bf9a3fd6955c
Nick Morris <nicholasmorris2599@gmail.com>
2023-04-17 14:09:19 -0700

    Mapping over all order items and adding lists.

--- client/src/components/PastOrderCard/index.js
+++ client/src/components/PastOrderCard/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import PastOrderItem from '../PastOrderItem';
 import PastOrderEmpty from '../PastOrderEmpty';
+import { useStoreContext } from '../../utils/GlobalState';
 
 function PastOrderCard() {
+
+    const [state, dispatch] = useStoreContext();
+
+    const orderNumber = state.order.length;
 
     return (
         // Will handle if order is empty
         <>
             <div className="account-order-block">
-                <div className="order">Order<span className="account-order-number"> 1 </span> </div>
+                <div className="order">Order<span className="account-order-number">{orderNumber}</span> </div>
                 <div className="account-divider"></div>
                 <ul className="account-order-items-list">
-                    <li className="account-qty"> 4 <span className="account-list">Lucky You</span></li>
-                    <li className="account-qty"> 3 <span className="account-list">Black and White </span></li>
-                    <li className="account-qty"> 3 <span className="account-list">Crunch Berries </span></li>
-                    <li className="account-qty"> 2 <span className="account-list">Sun-kissed Chocolate </span></li>
+                    {state.order.map((donut) => (
+                        <PastOrderItem key={donut._id} donut={donut}/>
+                    ))}
                 </ul>
                 <div className="account-divider"></div>
                 <div className="account-box">
---
+++ client/src/components/PastOrderItem/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function PastOrderItem() {
+    return (
+        <>
+            <li className="account-qty"> 4 <span className="account-list">Lucky You</span></li>
+            <li className="account-qty"> 3 <span className="account-list">Black and White </span></li>
+            <li className="account-qty"> 3 <span className="account-list">Crunch Berries </span></li>
+            <li className="account-qty"> 2 <span className="account-list">Sun-kissed Chocolate </span></li>
+        </>
+    )
+};
+
+export default PastOrderItem;
